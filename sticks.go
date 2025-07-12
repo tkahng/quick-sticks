2507b6ccce3e39c167d4f5f580dc94003583717e
tkahng <tkahng@gmail.com>
2025-07-12 10:39:44 -0700

    add tests

--- sticks.go
+++ sticks.go
@@ -1,17 +1,64 @@
 package sticks
 
+import "errors"
+
 type Hand struct {
 	Fingers int // 0–4 (5+ becomes 0)
 }
 
+func (h *Hand) Attack(opp *Hand) error {
+	if opp.Fingers == 0 {
+		return errors.New("opponent has no fingers")
+	}
+	h.Fingers++
+	opp.Fingers--
+	return nil
+}
+
+func (h *Hand) Split(other *Hand) error {
+	if other.Fingers > 1 {
+		half := other.Fingers / 2
+		h.Fingers += half
+		other.Fingers -= half
+		return nil
+	}
+	return errors.New("cannot split. not enough other fingers")
+}
+
 type Player struct {
 	ID    string
-	Hands [2]Hand
+	Idx   int // 0 or 1
+	Hands [2]*Hand
 }
 
 type Game struct {
 	ID        string
-	Players   [2]Player
+	Players   [2]*Player
 	TurnIndex int    // 0 or 1
 	Status    string // "waiting", "playing", "finished"
+}
+
+func NewGame(id string) *Game {
+	return &Game{
+		ID:        id,
+		Players:   [2]*Player{},
+		TurnIndex: 0,
+		Status:    "waiting",
+	}
+}
+
+func (g *Game) AddPlayer(p *Player) error {
+	for idx, player := range g.Players {
+		if player == nil {
+			p.Idx = idx
+			g.Players[idx] = p
+			if idx == 1 {
+				g.Status = "playing"
+			} else {
+				g.Status = "waiting"
+			}
+			return nil
+		}
+	}
+	return errors.New("game is full")
 }
---
+++ sticks_test.go
@@ -0,0 +1,117 @@
+package sticks
+
+import (
+	"testing"
+)
+
+func TestModulo(t *testing.T) {
+	t.Run("5 divide by 2", func(t *testing.T) {
+		got := 7 / 2
+		want := 3
+		if got != want {
+			t.Errorf("Wanted %d but got %d", want, got)
+		}
+	})
+}
+
+func TestGame_AddPlayer(t *testing.T) {
+	type fields struct {
+		ID        string
+		Players   [2]*Player
+		TurnIndex int
+		Status    string
+	}
+	type args struct {
+		p   *Player
+		idx int
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "add player to empty game",
+			fields: fields{
+				ID:        "empty game",
+				Players:   [2]*Player{},
+				TurnIndex: 0,
+				Status:    "waiting",
+			},
+			args: args{
+				p: &Player{
+					ID:    "player 1",
+					Idx:   0,
+					Hands: [2]*Hand{},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "add player to waiting game",
+			fields: fields{
+				ID: "waiting game",
+				Players: [2]*Player{
+					{
+						ID:    "player 1",
+						Idx:   0,
+						Hands: [2]*Hand{},
+					},
+				},
+				TurnIndex: 0,
+				Status:    "waiting",
+			},
+			args: args{
+				p: &Player{
+					ID:    "player 1",
+					Idx:   0,
+					Hands: [2]*Hand{},
+				},
+				idx: 1,
+			},
+			wantErr: false,
+		},
+		{
+			name: "add player to full game",
+			fields: fields{
+				ID: "waiting game",
+				Players: [2]*Player{
+					{
+						ID:    "player 1",
+						Idx:   0,
+						Hands: [2]*Hand{},
+					},
+					{
+						ID:    "player 2",
+						Idx:   1,
+						Hands: [2]*Hand{},
+					},
+				},
+				TurnIndex: 0,
+				Status:    "waiting",
+			},
+			args: args{
+				p: &Player{
+					ID:    "player 1",
+					Idx:   0,
+					Hands: [2]*Hand{},
+				},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			g := &Game{
+				ID:        tt.fields.ID,
+				Players:   tt.fields.Players,
+				TurnIndex: tt.fields.TurnIndex,
+				Status:    tt.fields.Status,
+			}
+			if err := g.AddPlayer(tt.args.p); (err != nil) != tt.wantErr {
+				t.Errorf("Game.AddPlayer() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
