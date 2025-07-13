082758eb1fc13251552a2bd826cee22be97e2fdd
tkahng <tkahng@gmail.com>
2025-07-13 15:50:26 -0700

    player id middleware

--- server/middleware.go
+++ server/middleware.go
@@ -1,6 +1,10 @@
 package server
 
-import "net/http"
+import (
+	"log/slog"
+	"net/http"
+	"time"
+)
 
 func Cors(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -9,6 +13,39 @@ func Cors(h http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Authorization, X-CSRF-Token")
 		w.Header().Set("Access-Control-Expose-Headers", "Link")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
+		h.ServeHTTP(w, r)
+	})
+}
+
+func PlayerID(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var playerID string
+		playerIDCookie, err := r.Cookie("player_id")
+		if err != nil {
+			slog.Error("Failed to get player ID cookie", slog.Any("error", err))
+			http.Error(w, "Failed to get player ID cookie", http.StatusInternalServerError)
+			return
+		}
+		if playerIDCookie.Value == "" {
+			playerID = generatePlayerID()
+		} else {
+			playerID = playerIDCookie.Value
+		}
+		// nolint:exhaustruct
+		cookie := &http.Cookie{
+			Name:     "my_session_cookie",
+			Value:    playerID,
+			Expires:  time.Now().Add(24 * time.Hour), // Cookie expires in 24 hours
+			Path:     "/",                            // Valid for all paths
+			HttpOnly: true,                           // Accessible only via HTTP(S), not JavaScript
+			Secure:   true,                           // Only sent over HTTPS
+			SameSite: http.SameSiteStrictMode,        // Strict SameSite policy
+		}
+
+		// Set the cookie on the response writer
+		http.SetCookie(w, cookie)
+
+		// Call the next handler in the chain
 		h.ServeHTTP(w, r)
 	})
 }
